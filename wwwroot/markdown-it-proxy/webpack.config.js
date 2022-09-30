6e00dda42b88bc4cbd1a4e5dbac988fcec11868d
capdiem <capdiem@live.com>
2022-09-30 18:04:40 +0800

    🚧 feat: wip

---
+++ wwwroot/markdown-it-proxy/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.ts",
+  mode: "development",
+  dev
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: [
+              [
+                "prismjs",
+                {
+                  languages: ["javascript", "css", "markup", "csharp", "sql"],
+                  // "plugins": ["line-numbers"],
+                  // "theme": "twilight",
+                  // "css": true
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    filename: "markdown-it-proxy.min.js",
+    module: true,
+    path: path.resolve(__dirname, "../js"),
+  },
+};
